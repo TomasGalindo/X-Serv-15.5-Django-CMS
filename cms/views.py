4b836231fcc6c1418bc8edfd75c9d51d7881f9cb
TomasGalindo <tgalindoencinas@yahoo.com>
2017-03-27 19:33:17 +0200

    Views Completo

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+from cms.models import Pages
+
+
+def pagina(request, identificador):
+    if request.method == "GET":
+        try:
+            pagina = Pages.objects.get(id=identificador)
+            respuesta = pagina.page
+        except Page.DoesNotExist:
+            respuesta = "No existe la pagina " + str(identificador)
+
+    else:
+        respuesta = "No puedes utilizar ese metodo. Puedes utilizar GET"
+
+    return HttpResponse(respuesta)
